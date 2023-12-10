fbfdc06cbba038bbee931a6db3684c17b5f1497c
ndya18 <153446219+ndya18@users.noreply.github.com>
2023-12-11 00:34:44 +0700

    Add files via upload

---
+++ JOBSHEET 3 Variabel, Konstanta dan Tipe Data/LATIHAN/LATIHAN 2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//Created by Nadya_23343088
+
+int main(){
+	int a = 5;
+	int b = 2;
+	
+	float c = a / b;
+	
+	printf("Hasil a / b = %.2f\n", c);
+		
+}
---
+++ JOBSHEET 3 Variabel, Konstanta dan Tipe Data/LATIHAN/LATIHAN 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define SEPULUH 10
+#define VERSI 4.5
+#define JENIS_KELAMIN 'L'
+
+//Created by Nadya_23343088
+
+void main(){
+	printf("isi konstanta SEPULUH adalah %i\n", SEPULUH);
+	printf("isi konstanta VERSI adalah %f\n", VERSI);
+	printf("isi konstanta JENIS_KELAMIN adalah %c\n", JENIS_KELAMIN);
+	
+}
